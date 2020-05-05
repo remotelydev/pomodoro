a1b48108d675fce36ab02eeea662a8d46f6950a1
Bartosz Trzos <Bartosz.Trzos@ipfdigital.com>
2020-05-05 19:18:43 +0200

    Updates

---
+++ precache-manifest.850f633577d22b9dcedc0e767737d7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d696fe368d46dbef19e1378c97df632c",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bf8e9c4236d786e3b73f",
+    "url": "/calculator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9db48010f36937fab6b6",
+    "url": "/calculator/static/js/2.387ef03f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.387ef03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8e9c4236d786e3b73f",
+    "url": "/calculator/static/js/main.8acd3367.chunk.js"
+  },
+  {
+    "revision": "d60859d8a9f5f0e7455b",
+    "url": "/calculator/static/js/runtime-main.1fde7ec2.js"
+  }
+]);
